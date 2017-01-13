2bdfe416246bb348f6bfcfebda7454248e25f93a
Emanuil Stoykov <emanuilstoykov@yahoo.com>
2017-01-13 23:20:59 +0200

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,20 @@
+jQuery(function ($) {
+
+
+    $(document).ready(function () {
+        $("#b").click(function () {
+            $("#appear2").toggle();
+        });
+    });
+
+    $(document).ready(function () {
+        $("#c").click(function () {
+            $("#appear3").toggle();
+        });
+    });
+    $(document).ready(function () {
+        $("#a").click(function () {
+            $("#ap").toggle();
+        });
+    });
+});
